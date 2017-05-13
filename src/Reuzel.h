fc0de12c7947f1f8891f7471e22ebca5882945ff
leohotfn <leohotfn@qq.com>
2017-05-13 20:25:56 +0800

    Update Reuzel.h

--- src/Reuzel.h
+++ src/Reuzel.h
@@ -12,21 +12,24 @@
 #include <deque>
 #include <functional>
 
+using std::string;
+
 namespace Reuzel {
     class ThreadPool {
     public:
         typedef std::function<void ()> Task;
 
+        explicit ThreadPool(const string &nameArg = string("ThreadPool"));
     private:
         mutable pthread_mutex_t mutex_;
         pthread_cond_t notEmpty_;
         pthread_cond_t notFull_;
-        std::string name_;
+        string name_;
         Task threadInitCallback_;
         std::vector<pthread_t> threads_;
         std::deque<Task> queue_;
+        size_t maxQueueSize;
         bool running_;
-        size_t maxSize;
     };
 }
 
