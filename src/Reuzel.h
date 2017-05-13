a8ce526de50affe401b44fe82d9df4d182bfcacc
leohotfn <leohotfn@qq.com>
2017-05-13 19:52:33 +0800

    Updte Reuzel.h

--- src/Reuzel.h
+++ src/Reuzel.h
@@ -7,52 +7,26 @@
 #ifndef REUZEL_H
 #define REUZEL_H
 
-#include <vector>
-#include <string>
 #include <pthread.h>
-
-using std::string;
-using std::vector;
+#include <vector>
+#include <deque>
+#include <functional>
 
 namespace Reuzel {
-    class Task {
-    public:
-        Task();
-        Task(const string &data) : task_data(data)
-        {}
-        ~Task();
-
-        void setData(const string &data)
-        {
-            task_data = data;
-        }
-
-        void run(pthread_t pid);
-    private:
-        static int sign;
-        string task_data;
-    };
-
     class ThreadPool {
     public:
-        ThreadPool(int num);
-        ~ThreadPool();
-        void createPool();
-        static void *threadCall(void *data);
-        static void move2Busy(const pthread_t pid);
-        static void move2Idle(const pthread_t pid);
+        typedef std::function<void ()> Task;
 
-        static pthread_mutex_t output_mutex;
-        static vector<pthread_t> busy;
-        static vector<pthread_t> idle;
     private:
-        static vector<Task *> task_list;
-        static bool shutdown;
-        int thread_num;
-        pthread_t *ptid;
-        static pthread_mutex_t mutex;
-        static pthread_cond_t cond;
-        static pthread_mutex_t task_list_mutex;
+        mutable pthread_mutex_t mutex_;
+        pthread_cond_t notEmpty_;
+        pthread_cond_t notFull_;
+        std::string name_;
+        Task threadInitCallback_;
+        std::vector<pthread_t> threads_;
+        std::deque<Task> queue_;
+        bool running_;
+        size_t maxSize;
     };
 }
 
