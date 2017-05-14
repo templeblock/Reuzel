3783c50a6a2313f06cf7ea7271725cb33f6b457a
leohotfn <leohotfn@qq.com>
2017-05-14 11:21:12 +0800

    Update Reuzel.cpp

--- src/Reuzel.cpp
+++ src/Reuzel.cpp
@@ -3,3 +3,17 @@
 //
 // Copyright (c) 2017 Jiawei Feng
 //
+
+#include "Reuzel.h"
+
+using namespace Reuzel;
+
+ThreadPool::ThreadPool(const string &nameArg)
+{
+    pthread_mutex_init(&mutex_, NULL);
+    pthread_cond_init(&notEmpty_, NULL);
+    pthread_cond_init(&notFull_, NULL);
+    name_ = nameArg;
+    maxQueueSize_ = 0;
+    running_ = false;
+}
