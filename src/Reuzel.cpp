c406faea0c9afb64fb91c39569677167d129ffbf
leohotfn <leohotfn@qq.com>
2017-05-14 10:26:39 +0800

    Update

--- src/Reuzel.cpp
+++ src/Reuzel.cpp
@@ -3,20 +3,3 @@
 //
 // Copyright (c) 2017 Jiawei Feng
 //
-
-#include "Reuzel.h"
-#include <iostream>
-#include <vector>
-#include <assert.h>
-
-using namespace Reuzel;
-
-ThreadPool::ThreadPool(const string &nameArg)
-{
-    pthread_mutex_init(&mutex_, NULL);
-    pthread_cond_init(&notEmpty_, NULL);
-    pthread_cond_init(&notFull_, NULL);
-    name_ = nameArg;
-    maxQueueSize = 0;
-    running_ = false;
-}
--- src/Reuzel.h
+++ src/Reuzel.h
@@ -7,30 +7,5 @@
 #ifndef REUZEL_H
 #define REUZEL_H
 
-#include <pthread.h>
-#include <vector>
-#include <deque>
-#include <functional>
-
-using std::string;
-
-namespace Reuzel {
-    class ThreadPool {
-    public:
-        typedef std::function<void ()> Task;
-
-        explicit ThreadPool(const string &nameArg = string("ThreadPool"));
-    private:
-        mutable pthread_mutex_t mutex_;
-        pthread_cond_t notEmpty_;
-        pthread_cond_t notFull_;
-        string name_;
-        Task threadInitCallback_;
-        std::vector<pthread_t> threads_;
-        std::deque<Task> queue_;
-        size_t maxQueueSize;
-        bool running_;
-    };
-}
 
 #endif
